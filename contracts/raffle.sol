f4897b56938cde9c7b4338b7caf3903d53e0eafc
Ryley Ziegler <ryleyziegler@Ryleys-MacBook-Pro.local>
2022-11-04 23:19:00 -0400

    Raffle contract.

---
+++ contracts/raffle.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+import "hardhat/console.sol";
+
+
+contract Raffle {
+    address[] users;
+    
+    function checkIfUserExists(address user) private view returns (bool){
+        uint i = 0;
+        if( users.length >= 1){
+            while (i < users.length){
+                if(user != users[i])
+                    i+=1;
+                else
+                    return true;
+            }
+            return false;
+        }
+        else 
+            return false;
+    }
+    // Adds user to raffle if they dont already exist.
+    function joinRaffle(address user) public {
+        if (!checkIfUserExists(user))
+            users.push(user);
+        else
+            console.log("User cannot join raffle twice.");
+    }
+
+    // Gets all users in raffle.
+    function getUsersInRaffle() public view returns (address[] memory){
+        return users;
+    }
+
+}
