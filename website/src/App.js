071db0c7ff8b3f0b15ddd2b45120ede0ca035135
Ibana Smith <ismith9@students.towson.edu>
2022-11-13 18:36:15 -0500

    Created Navbar and styling

--- website/src/App.js
+++ website/src/App.js
@@ -1,10 +1,9 @@
-import './Site.css';
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <div className="App">
-<Navbar/>
+            <Navbar/>
         </div>
     );
 }
