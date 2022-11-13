e68eddefed3cf6e3d3b57d4c75ad2a6cad47f6a2
iansmith1337 <iansmith1337@gmail.com>
2022-11-13 13:41:44 -0500

    Add reset script and add user weighting by number of tickets

--- contracts/raffle.sol
+++ contracts/raffle.sol
@@ -8,23 +8,21 @@ import "@openzeppelin/contracts/security/PullPayment.sol";
 // Used to join a raffle for a geo-coin NFT.
 contract Raffle is Ownable, PullPayment {
     mapping (address => User) private _users;
-    address payable private withdrawAddress;
-    uint256 private _cost = 0.25 ether;
+    uint256 private _cost = 0.01 ether;
     address[] public registeredUsers;
     address[] public awardedUsers;
+    address[] private weighted;
     
     struct User {
         address ID;
-        bool isRegisteredForRaffle;
+        uint tickets;
     }
 
-    error InsufficientPayment(uint raffleCost);
+    error UnevenPayment(uint tickets, uint raffleCost);
     error InvalidAddress(address addr);
     error duplicateWinner(address addr);
 
-    constructor () {
-        withdrawAddress = payable(owner());
-    }
+    constructor () {}
 
     // Ensures user exists for current sender.
     function checkIfUserExists() private view returns (bool){
@@ -34,26 +32,52 @@ contract Raffle is Ownable, PullPayment {
             return false;
     }
 
-    function checkIfUserIsRegistered() private view returns (bool){
-        return _users[msg.sender].isRegisteredForRaffle;
+    function checkIfUserIsRegistered() private view returns (bool) {
+        for (uint256 i = 0; i < registeredUsers.length; i++) {
+            if(msg.sender == registeredUsers[i]) 
+                return true;
+        }
+        return false;
     }
 
-    // Creates user and buys into the raffle.
-    function addUserAndBuyRaffleTicket() public payable {
+    // Creates user.
+    function addUser() public {
         require(!checkIfUserExists(), "Error: Cannot add duplicate user to contract.");
-        _users[msg.sender] = User(msg.sender, false);
-        if (msg.value != _cost)
-            revert InsufficientPayment({
+        _users[msg.sender] = User(msg.sender, 0);
+    }
+
+    // Allow user to buy into raffle
+    function buyTickets(uint t) public payable {
+        require(checkIfUserExists(), "Error: User doesn't exist yet.");
+        require((_users[msg.sender].tickets += t) > _users[msg.sender].tickets, "Error: Ticket overflow.");
+        if (msg.value % _cost > 0)
+            revert UnevenPayment({
+                tickets: t,
                 raffleCost: _cost
             });
         else
-            registeredUsers.push(msg.sender);
-            _users[msg.sender].isRegisteredForRaffle = true;
+            if(checkIfUserIsRegistered())
+                _users[msg.sender].tickets += t;
+            else
+                registeredUsers.push(msg.sender);
+                _users[msg.sender].tickets = t;
     }
 
-    // Gets all users in raffle.
-    function getUsersInRaffle() public view onlyOwner returns (address[] memory){
-        return registeredUsers;
+    // Calculator func for weighted array
+    // registeredUsers = {0x1, 0x2, 0x3, 0x4}
+    // tickets = 1, 2, 3, 4
+    // weighted = {0x1, 0x2, 0x2, 0x3, 0x3, 0x3, 0x4, 0x4, 0x4, 0x4}
+    function calcWeightedArrayFromRaffle() private onlyOwner {
+        for (uint256 i = 0; i < registeredUsers.length; i++) {
+            for (uint256 j = 0; j < _users[registeredUsers[i]].tickets; j++) {
+                weighted.push(registeredUsers[i]);
+            }
+        }
+    }
+
+    // Gets weighted users from the raffle.
+    function getWeightedFromRaffle() public view onlyOwner returns (address[] memory){
+        return weighted;
     }
 
     // Gets all winners from the raffle.
@@ -75,5 +99,14 @@ contract Raffle is Ownable, PullPayment {
     /// @dev Overridden in order to make it an onlyOwner function
     function withdrawPayments(address payable payee) public override onlyOwner virtual {
         super.withdrawPayments(payee);
+    }
+
+    function reset() private onlyOwner {
+        for (uint256 i = 0; i < registeredUsers.length; i++) {
+            _users[registeredUsers[i]].tickets = 0;
+        }
+        delete registeredUsers;
+        delete awardedUsers;
+        delete weighted;
     }
 }
