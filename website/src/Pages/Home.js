58fa1b551c14cd8a279bf08d6e27a8d548432716
Ryley Ziegler <ryleyziegler@Ryleys-MacBook-Pro.local>
2022-12-08 15:57:49 -0500

    Fixed abi's (wrong format) and gave register button functionality.

--- website/src/Pages/Home.js
+++ website/src/Pages/Home.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import abi from "./utils/GCW3Raffle.json";
 const { ethers } = require("ethers");
-import abi from "./utils/GWC3Raffle_metadata.json";
-
-function clickMe(){
-    alert('You clicked me!');
-}
 
 export default function Home() {
 
+    // Raffle Contract.
+    const contractAddress = "0x57b376e2087acae095440da50d443468b797c903"
+    const contractABI = abi.abi;
+
+    const register = async() => {
+
+        try {
+          const { ethereum } = window;
+    
+        if (ethereum){
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const raffleContract = new ethers.Contract(contractAddress, contractABI, signer);
+          
+          await raffleContract.addUser()
+
+        }
+      } catch (error) {
+        console.log("error")
+      }
+      }
+
     return (
         <div>
-            <button onClick={clickMe}>
+            <button onClick={register}>
                 Join Raffle
             </button>
         </div>
