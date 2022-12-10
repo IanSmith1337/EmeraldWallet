29b031c0639f1f4833d0fcb1290c2a7b6edf8eb6
Ryley Ziegler <ryleyziegler@Ryleys-MacBook-Pro.local>
2022-12-10 12:23:26 -0500

    Some more layout design and css.

--- website/src/Pages/Home.js
+++ website/src/Pages/Home.js
@@ -29,17 +29,33 @@ export default function Home() {
 
     return (
         <div>
-          <div className='containers'>
-            <button className='btns' onClick={register}>
-                Join Raffle
-            </button>
-            <button className='btns' onClick={''}> 
-                Buy Tickets
-            </button>
-            <p className='formfield'>
-            <label className ='black-text' for='textarea'>Amount of Tickets: </label>
-            <textarea id='textarea' className ='txts' onChange={(e) => setText(e.target.value)} value={text}></textarea>
-            </p>
+          <div className='row'>
+            <div className='containers'>
+              <h2>Welcome to Web3 Geocaching!</h2>
+              <h3>How to use the site:</h3>
+              <h4>Connect Wallet.</h4>
+              <h4>Sign up for raffle.</h4>
+              <h4>Buy tickets for a higher chance to win.</h4>
+              <h4>Reap the rewards of a GeoCoin NFT.</h4>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='containers3'>
+              <label>Click here to join raffle.</label>
+              <button className='btns' onClick={register}>
+                      Join Raffle
+                </button>
+            </div>
+            <div className='containers2'>
+              <label>Enter the amount of tickets you want and click here.</label>
+              <button className='btns' onClick={''}> 
+                  Buy Tickets
+              </button>
+              <p className='formfield'>
+              <label className ='black-text' for='textarea'>Amount of Tickets: </label>
+              <textarea id='textarea' className ='txts' onChange={(e) => setText(e.target.value)} value={text}></textarea>
+              </p>
+            </div>
           </div>
         </div>
 
