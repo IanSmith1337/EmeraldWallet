7f47c8180210207614807711ab309617c4bccf90
Ryley Ziegler <ryleyziegler@Ryleys-MacBook-Pro.local>
2022-12-10 00:33:50 -0500

    Start to buy tickets functionality.

--- website/src/Pages/Home.js
+++ website/src/Pages/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import abi from "./utils/GCW3Raffle.json";
 const { ethers } = require("ethers");
+const {useState} = require("react");
 
 export default function Home() {
 
     // Raffle Contract.
     const contractAddress = "0x57b376e2087acae095440da50d443468b797c903"
     const contractABI = abi.abi;
-
+    const [text, setText] = useState("");
     const register = async() => {
 
         try {
@@ -28,9 +29,18 @@ export default function Home() {
 
     return (
         <div>
-            <button onClick={register}>
+          <div className='containers'>
+            <button className='btns' onClick={register}>
                 Join Raffle
             </button>
+            <button className='btns' onClick={''}> 
+                Buy Tickets
+            </button>
+            <p className='formfield'>
+            <label className ='black-text' for='textarea'>Amount of Tickets: </label>
+            <textarea id='textarea' className ='txts' onChange={(e) => setText(e.target.value)} value={text}></textarea>
+            </p>
+          </div>
         </div>
 
     )
