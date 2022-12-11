e9b4fa95ce8ef80e8efe7e0aeff5ba868790bc83
Ryley Ziegler <ryleyziegler@Ryleys-MacBook-Pro.local>
2022-12-11 13:57:29 -0500

    Start to countdown.

--- website/src/Pages/Home.js
+++ website/src/Pages/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import abi from './utils/GCW3Raffle.json'
+import ReactDOM from 'react-dom';
+import Countdown from 'react-countdown';
 const { ethers } = require('ethers')
 const { useState } = require('react')
 
@@ -53,41 +55,62 @@ export default function Home() {
     }
   }
 
+  const Completionist = () => <span>You are good to go!</span>;
+
+  // Renderer callback with condition
+  const renderer = ({ days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+      // Render a completed state
+      return <Completionist />;
+    } else {
+      // Render a countdown
+      return <span>{days}:{hours}:{minutes}:{seconds}</span>;
+    }
+  };
+
   return (
     <div>
-      <div className="row">
-        <div className="containers">
-          <h2>Welcome to Web3 Geocaching!</h2>
-          <h3>How to use the site:</h3>
-          <h4>Connect Wallet.</h4>
-          <h4>Sign up for raffle.</h4>
-          <h4>Buy tickets for a higher chance to win.</h4>
-          <h4>Reap the rewards of a GeoCoin NFT.</h4>
+      <Countdown
+        date={Date.now() + 10000} 
+        intervalDelay={0}
+        precision={3}
+        renderer={renderer}
+      />
+      <div>
+        <div className="row">
+          <div className="containers">
+            <h2>Welcome to Web3 Geocaching!</h2>
+            <h3>How to use the site:</h3>
+            <h4>Connect Wallet.</h4>
+            <h4>Sign up for raffle.</h4>
+            <h4>Buy tickets for a higher chance to win.</h4>
+            <h4>Reap the rewards of a GeoCoin NFT.</h4>
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="containers3">
-          <label>Click here to join raffle.</label>
-          <button className="btns" onClick={register}>
-            Join Raffle
-          </button>
-        </div>
-        <div className="containers2">
-          <label>Enter the amount of tickets you want and click here.</label>
-          <button className="btns" onClick={() => buyTicket(text)}>
-            Buy Tickets
-          </button>
-          <p className="formfield">
-            <label className="black-text" htmlFor="textarea">
-              Amount of Tickets:{' '}
-            </label>
-            <textarea
-              id="textarea"
-              className="txts"
-              onChange={(e) => setText(e.target.value)}
-              value={text}
-            ></textarea>
-          </p>
+        <div className="row">
+          <div className="containers3">
+            <label>Click here to join raffle.</label>
+            <button className="btns" onClick={register}>
+              Join Raffle
+            </button>
+          </div>
+          <div className="containers2">
+            <label>Enter the amount of tickets you want and click here.</label>
+            <button className="btns" onClick={() => buyTicket(text)}>
+              Buy Tickets
+            </button>
+            <p className="formfield">
+              <label className="black-text" htmlFor="textarea">
+                Amount of Tickets:{' '}
+              </label>
+              <textarea
+                id="textarea"
+                className="txts"
+                onChange={(e) => setText(e.target.value)}
+                value={text}
+              ></textarea>
+            </p>
+          </div>
         </div>
       </div>
     </div>
