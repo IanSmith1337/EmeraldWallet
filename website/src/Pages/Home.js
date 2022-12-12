79815a2ed112f0b044524c42af74ab8f201ea5fe
iansmith1337 <iansmith1337@gmail.com>
2022-12-12 15:21:13 -0500

    Hopefully fix the infinite loop error

--- website/src/Pages/Home.js
+++ website/src/Pages/Home.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import abi from './utils/GCW3Raffle.json'
-import ReactDOM from 'react-dom';
-import Countdown from 'react-countdown';
+import ReactDOM from 'react-dom'
+import Countdown from 'react-countdown'
 const { ethers } = require('ethers')
 const { useState, useEffect } = require('react')
 
 export default function Home() {
   // Raffle Contract.
-  const contractAddress = '0x57b376e2087acae095440da50d443468b797c903'
+  const contractAddress = '0xC0FE64678c97B62D24F33D414686cbAba7471E79'
+  const contractAddressMinter = '0x9849a8C5060Ab5E69827b5B044B8E09E90692754'
   const contractABI = abi.abi
   const [text, setText] = useState('')
   const [admin, setAdmin] = useState(false)
-  const [registered,setRegistered] = useState('')
+  const [registered, setRegistered] = useState('')
 
   const register = async () => {
     try {
@@ -60,13 +61,12 @@ export default function Home() {
 
   const checkIfAdmin = async () => {
     try {
-      const {ethereum} = window
-      const address = await ethereum.request({method: "eth_accounts"});
+      const { ethereum } = window
+      const address = await ethereum.request({ method: 'eth_accounts' })
       console.log(address)
-      if (address.length == ' 0xA24b6Cab97696c22954DAEbd4747C2B57839FB2F') 
+      if (address.length == ' 0xA24b6Cab97696c22954DAEbd4747C2B57839FB2F')
         setAdmin(true)
-      else
-        console.log("Regular user")
+      else console.log('Regular user')
     } catch (error) {
       console.log('error')
     }
@@ -92,21 +92,23 @@ export default function Home() {
     }
   }
 
-  const pullWinners = async function (num, wA, contract) {
+  const pullWinners = async function (num, wA, contract, minter) {
     let wins = await contract.getWinnersFromRaffle()
-    let count = wins.length + num
-    while (wins.length < count) {
+    let current = 0
+    let end = num
+    let max = minter.getSupply()
+    while (current < end && wins.length < max) {
       try {
         var addr = wA[Math.floor(Math.random() * wA.length)]
         await contract.setWinnersFromRaffle(addr)
         wins = await contract.getWinnersFromRaffle()
+        current++
       } catch (error) {
         continue
       }
     }
     return wins
   }
-
 
   const awardAndMint = async () => {
     try {
@@ -120,11 +122,21 @@ export default function Home() {
           contractABI,
           signer,
         )
-        
+        const minterContract = new ethers.Contract(
+          contractAddressMinter,
+          contractABI,
+          signer,
+        )
+
         await raffleContract.calcWeightedArrayFromRaffle()
         let weighted = await raffleContract.getWeightedFromRaffle()
         const wArray = await weighted
-        var winnersList = await pullWinners(1, wArray, raffleContract)
+        var winnersList = await pullWinners(
+          1,
+          wArray,
+          raffleContract,
+          minterContract,
+        )
         console.log(' ')
         if (winnersList.length > 0) {
           console.log('Drawing #1 Winners:')
@@ -133,68 +145,71 @@ export default function Home() {
             console.log(win)
           })
         }
-        
       }
-    
     } catch (error) {
       console.log('error')
     }
   }
 
   useEffect(() => {
-    checkIfAdmin();
+    checkIfAdmin()
   }, [])
 
   useEffect(() => {
-    getRegistered();
+    getRegistered()
   }, [])
 
-  
   //remember to flip admin.
   return (
     <div>
-      {!admin && (<div>
-        <div className="row">
-          <div className="containers">
-            <h2>Welcome to Web3 Geocaching!</h2>
-            <h3>How to use the site:</h3>
-            <h4>Connect Wallet.</h4>
-            <h4>Sign up for raffle.</h4>
-            <h4>Buy tickets for a higher chance to win.</h4>
-            <h4>Reap the rewards of a GeoCoin NFT.</h4>
+      {!admin && (
+        <div>
+          <div className="row">
+            <div className="containers">
+              <h2>Welcome to Web3 Geocaching!</h2>
+              <h3>How to use the site:</h3>
+              <h4>Connect Wallet.</h4>
+              <h4>Sign up for raffle.</h4>
+              <h4>Buy tickets for a higher chance to win.</h4>
+              <h4>Reap the rewards of a GeoCoin NFT.</h4>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="containers3">
-            <label>Click here to join raffle.</label>
-            <button className="btns" onClick={register}>
-              Join Raffle
-            </button>
-          </div>
-          <div className="containers2">
-            <label>Enter the amount of tickets you want and click here.</label>
-            <button className="btns" onClick={() => buyTicket(text)}>
-              Buy Tickets
-            </button>
-            <p className="formfield">
-              <label className="black-text" htmlFor="textarea">
-                Amount of Tickets:{' '}
+          <div className="row">
+            <div className="containers3">
+              <label>Click here to join raffle.</label>
+              <button className="btns" onClick={register}>
+                Join Raffle
+              </button>
+            </div>
+            <div className="containers2">
+              <label>
+                Enter the amount of tickets you want and click here.
               </label>
-              <textarea
-                id="textarea"
-                className="txts"
-                onChange={(e) => setText(e.target.value)}
-                value={text}
-              ></textarea>
-            </p>
+              <button className="btns" onClick={() => buyTicket(text)}>
+                Buy Tickets
+              </button>
+              <p className="formfield">
+                <label className="black-text" htmlFor="textarea">
+                  Amount of Tickets:{' '}
+                </label>
+                <textarea
+                  id="textarea"
+                  className="txts"
+                  onChange={(e) => setText(e.target.value)}
+                  value={text}
+                ></textarea>
+              </p>
+            </div>
           </div>
         </div>
-      </div>)}
-      {admin && (<div>
-        <button onClick={awardAndMint}> Select Winners and Min Nfts</button>
-        <h1> Currently Registered:</h1>
-        <p>{registered}</p>
-      </div>)}
+      )}
+      {admin && (
+        <div>
+          <button onClick={awardAndMint}> Select Winners and Min Nfts</button>
+          <h1> Currently Registered:</h1>
+          <p>{registered}</p>
+        </div>
+      )}
     </div>
   )
 }
